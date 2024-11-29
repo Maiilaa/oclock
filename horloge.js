b6a0b456f616c8c47c478a9247d0a60146252f72
Maiilaa <zirarimaila8@gmail.com>
2024-11-29 14:47:55 +0100

    horloge

---
+++ horloge.js
@@ -0,0 +1,7 @@
+function updateClock() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById("clock").innerText = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
---
+++ horloge.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horloge</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Horloge</h1>
+    <span id="clock">00:00:00</span>
+
+    <a href="index.php">Retour au menu</a>
+
+    <script src="horloge.js"></script>
+</body>
+</html>
