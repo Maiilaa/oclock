a03f8342641fab6737aeb218cd023e394db07b12
Maiilaa <zirarimaila8@gmail.com>
2024-11-29 11:53:35 +0100

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oclock - Menu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Oclock</h1>
+    <div id="menu">
+        <a href="chronometre.html">Chronomètre</a>
+        <a href="horloge.html">Horloge</a>
+        <a href="minuteur.html">Minuteur</a>
+        <a href="reveil.html">Réveil</a>
+    </div>
+</body>
+</html>
